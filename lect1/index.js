b3e494811b79ebc40e52ab1b21b509ca8a0eb8ee
Rupesh155 <rudhg35@gmail.com>
2025-05-20 22:08:34 +0530

    js

---
+++ lect1/index.js
@@ -0,0 +1,44 @@
+console.log("hello");
+    // console.log("heheh");
+//     Number => 1    1.5 
+//     String   'c'  "c"
+//  "ccc"  
+// Boolean    true false  
+// undefined vs null
+// BigInt 
+// Symbol
+// int a = 5;
+//  var , let ,const
+// var a =5;
+// var a=10;
+// console.log(a);
+// let  b=10
+// b=12
+// console.log(b);
+// const a=10;
+// console.log(a);
+// let a
+// console.log(a);
+
+// undefined           vs              null
+
+// var a=null
+// var c="hello" + "hiii"
+// console.log(a);
+// var a="hello"
+// var b="byee"
+// console.log(a+"     " +b);
+
+// var firstName="aman"
+// var lastName   
+// + - * / %   = == === 
+// console.log(   5 == 5 )
+
+// var a=100
+// var b = a==100 
+// console.log(b);
+// console.log(true == " ");
+// var a=5 //=> "number"
+// console.log(typeof(typeof(a)))
+
+console.log(5-"5");// "55"
---
+++ lect2/index.js
@@ -0,0 +1,99 @@
+// console.log(5+5);//10
+// console.log(5 + "5");//"55"
+// console.log(5-"5");//0
+// // && ||
+// console.log(true && false && true&& true);
+// console.log(true || false || true|| true);
+// console.log(false &&  true);
+// 5=> 101
+// 4=> 100     100
+// & 
+// console.log(5&4);
+// 5=> 1 0 1
+// 4=> 1 0 0      100
+// let isBool=false
+
+// if("   "){
+//     console.log("hello");
+  
+// }
+// else{
+//     console.log('hiiii');
+   
+// }
+    // let data=  prompt("Enter your value")
+    // console.log(+data);
+// let a=67
+// let m=50
+//       if(a>70 && m >75){
+//         console.log("A");      
+//       }
+//       else if(a>70 || m<50){
+//         console.log("passs");
+//       }
+//       else{
+//         console.log("failssssss");
+//       }
+// let isBool=true
+// isBool?console.log("helll0"):console.log("hiiii");
+// console.log(5-"hello");
+// 5-hello
+// console.log(typeof NaN);
+// let a=5  
+
+// Object ,Array
+
+// let name1="aanya"
+// let name2="afroz"
+// let nam3="anu"
+
+const student= {
+name1:"anya",
+name2:"sakshi",
+nam3:"ritik",
+age:35
+}
+student.age=20
+// console.log(student.name1);
+// console.log(student.age);
+//       let a={name:"ritik"}  // => 5
+//         // let b={name:"ritik"} //=> 1
+//         let b=a
+
+// console.log( b==a );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
