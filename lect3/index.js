10062136b35331a43d71d385f31338d8e0745040
Rupesh155 <rudhg35@gmail.com>
2025-05-21 21:55:18 +0530

    lect3

---
+++ lect3/index.js
@@ -0,0 +1,127 @@
+// let arr=[1,2,3,4,"hello",false]
+// console.log(arr[3]);
+// console.log("hiiii");
+// console.log("hiiii");
+// console.log("hiiii");
+// console.log("hiiii");
+// console.log("hiiii");
+// console.log("hiiii");
+// function sum(a=20,b=293){
+//     console.log(a+b);
+// }
+// sum(10,202020)
+// let show=(a) => {
+//     console.log(a+b);
+// }
+// show(2)
+
+// for(let i=9;i>0;i--){
+//     console.log(i);
+// }
+// let arr=[1,2,3,4,5,6,67,8]
+// let newA=[]
+
+//let newArr=[4,5,6,67,8]
+// arr.push(100)
+
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]>3){
+//         newA.push(arr[i])
+//     }  
+// }
+// console.log(newA);
+
+// let arr=[1,2,3,4,5,5,56]
+
+// for(let i of arr){
+//     console.log(i);
+
+// }
+// for(let i in arr){
+//     console.log(i);
+
+// }
+
+// let arr=[1,2,3,4,5,6]
+// let count=0
+// function sum(data){
+//     // console.log(data);
+//     for(let i of data){
+//         count=count+i
+//     }
+//     console.log(count);
+// }
+// sum(arr)
+// console.log([]==[]);
+
+// let arr=[ {id:1,name:"rahul"},{id:2,name:"rohit"} ]
+// for(let i   of arr){
+//     console.log(i.id);
+// }
+// let obj={
+//     id:1,name:"rahul"
+// }
+// function sum(data){
+//     console.log(data.id);
+    
+
+// }
+// sum(obj)
+
+// let obj={
+//     id:1,
+//     name:"rahul",
+//     lastName:"jain",
+//     fullName:function(){
+//         console.log(obj.name+ "    "+obj.lastName);
+//     }
+// }
+// obj.fullName()
+// let arr=[1,2,3,4,5,6]
+// arr.push(100)
+// console.log(arr);
+// arr.pop()
+// console.log(arr);
+// arr.unshift(10)
+// console.log(arr);
+// arr.shift()
+// console.log(arr);
+
+// let arr=[1,2,3,4,5]
+// let target =3
+//  let data=  arr.find( (a)=>{
+//     return a==3
+// })
+// console.log(data);
+
+function sum(){
+    return 5
+    console.log("hiiiii");
+    
+}
+
+console.log(sum());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
