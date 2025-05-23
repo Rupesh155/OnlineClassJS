3dc6e8cea51e4fceb82ac96efc620a3f3f5b3857
Rupesh155 <rudhg35@gmail.com>
2025-05-23 21:53:36 +0530

    methodes

---
+++ lect5/index.js
@@ -0,0 +1,117 @@
+// let arr=["apple","banana","cherry"]
+// arr.map((data)=>{
+//     console.log(data[0]);
+    
+
+// })
+
+// let arr=[1,2,3,4,4,5,6,7,3]
+// let arr1=[1,2]
+// let merge=[]
+
+// for(let i of arr){
+//     merge.push(i)
+// }
+// merge[1,2,3,4,4,5,6,7,3,1,2]
+
+
+// for(let i of arr1){
+//     merge.push()
+// }
+
+
+
+
+
+
+// console.log(        );
+// let luckey= Math.floor( Math.random()*10)
+
+let arr=[-3,0,3,5,5,2,0,1]
+
+
+// for(let i=0;i<5;i++){
+//     for(let j=0;j<5;j++){
+//         console.log("*");
+        
+//     }
+// }
+
+
+// ******     888888
+               88888
+                8888
+                 888
+                  88
+                   8
+// ****
+// ***
+// **
+// *
+
+
+
+
+
+
+
+
+// let merge=[...arr1,...arr]
+// console.log(merge);
+
+
+
+// let merge=[]
+// for(let i of arr){
+//     merge.push(i)
+// }
+   
+//   let maxValue=   arr.reduce((a,b)=>{
+//     return a>b?a:b
+
+//    })
+
+//   let newArr=   arr.filter((a,b)=>{
+//     return a!==maxValue
+
+//    })
+//   let maxB=  newArr.reduce((a,b)=>{
+//     return a>b?a:b
+
+//    })
+//    console.log(maxB);
+   
+
+
+// let arr1=[2,43,12,4,2,5]
+// 2nd max => 
+
+
+// console.log(arr.includes(3));
+// console.log(arr.indexOf(100));
+
+
+// let data= arr.find((a,b,c)=>{
+//     return a==3
+
+// })
+// console.log(data);
+// let data1= arr.filter((a,b,c)=>{
+//     return a==3
+
+// })
+// console.log(data1)
+
+// //  let data=  arr.some((a)=>{
+// //     return a==5 
+    
+// // })
+// // console.log(data);
+// // let data1=  arr.every((a)=>{
+// //     return a==5 
+    
+// // })
+// // console.log(data);
+
+
+
