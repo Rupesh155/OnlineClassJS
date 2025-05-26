74e401fd2771db7d061dc5331d5bc4be01b1dacb
Rupesh155 <rudhg35@gmail.com>
2025-05-26 22:16:06 +0530

    2D

---
+++ lect7/index.js
@@ -0,0 +1,50 @@
+// let arr=[1,2,3,4,5,56,6]
+// console.log(arr.length);
+// console.log(arr[0]);
+
+
+// 2D
+// let arr=[2,45,23,45,77,4]
+// let data=  arr.reduce((a,b)=>{
+//             return a+b=> a=a+b
+// },0)
+// console.log(data);
+
+let arr1=[ [1,2,3,4,5],[6,7,8,9,0] ]  
+
+console.log(arr1.flat() );
+
+
+// let totalSum= arr1.reduce((sum,b)=>{
+//     return sum+b.reduce((val,res)=>{ return val+res},0)
+// sum = sum+b.reduce((val,res)=>{ return val+res},0)
+
+// },0)
+// console.log(totalSum);
+
+
+
+
+
+
+
+    //   let data= arr1.map((a,b,c)=>a.map((res)=> {return res*2}))
+    //   console.log(data.flat());
+      
+
+// console.log(arr1.length);
+// console.log(arr1[0][1]);
+
+// for(let i=0;i<arr1.length;i++){
+//     for(let j=0;j<arr1[i].length;j++){
+//         console.log(arr1[i][j]);
+        
+//     }
+// }
+
+// i=0  true 
+// j=0  1<5 
+// console.log(arr1[0][1]);
+
+
+
