572d21e9a87491ae884edf61e9618354789fa142
Rupesh155 <rudhg35@gmail.com>
2025-05-28 20:38:23 +0530

    String

---
+++ lect6/index.js
@@ -0,0 +1,111 @@
+
+
+
+
+// console.log("Original String: '" + str + "'");
+
+// // 1. length
+// console.log("Length:", str.length); // includes spaces
+
+// // 2. trim
+// console.log("Trimmed:", str.trim()); // removes spaces
+
+// // 3. toUpperCase
+
+
+// // 5. includes
+// console.log("Includes 'Awesome':", str.includes("awesome"));
+
+// // 6. startsWith
+// console.log("Starts with '  Java':", str.startsWith("Java"));
+
+// // 7. endsWith
+// console.log("Ends with '!  ':", str.endsWith("!  "));
+
+// // 8. indexOf
+// console.log("Index of 'is':", str.indexOf("is"));
+
+
+ 
+// // 10. charAt
+// console.log("Character at index 5:", str.charAt(5));
+
+// // 11. substring (from index 2 to 10)
+
+
+// // 12. slice
+
+// // 13. split
+
+// // 14. replace
+// console.log("Replace 'Awesome' with 'Powerful':", str.replace("Awesome", "Powerful"));
+// // 15. repeat
+// console.log(str.repeat(3));
+// // 9. lastIndexOf
+// console.log("Last index of 'a':", str.lastIndexOf("a"));
+// console.log("Uppercase:", str.toUpperCase());
+// // 4. toLowerCase
+
+
+
+// // console.log("Lowercase:", str.toLowerCase());
+// console.log("Slice (2, -1):", str.slice(2, -1));
+// str=" a aa bbbbccccaa"; =>  a3b4c4a2
+// let str="aaabbccaa";    //a3b2c2    abc
+// let newStr="a"
+// for(let i=0;i<str.length;i++){
+//     if(!newStr.includes(str[i])){
+//         newStr+=str[i]
+//     }
+// }
+// console.log(newStr);
+// let count =1;
+
+// for(let i=0;i<str.length;i++){
+//     if(str[i]==str[i+1]){
+//         count++
+
+//     }else{
+//         newStr+=str[i] 
+//         count=1  
+   
+//     }
+
+// }
+// console.log(newStr);
+// let str="hello my name is khan"
+// console.log(str.split(""));
+
+
+// let arr=[1,2,3,4,5,6]
+// let arr1=[2,3,5,2,5,6,10]
+
+
+// let output=[1,2,3,4,5,6,10]
+
+// let arr=[1,2,3,4,5,-6,8,8]
+
+//  let firstMax=   Math.max(...arr)
+//            let max=    arr.find((a)=>{
+//                 return a==firstMax
+
+//                })
+
+//  console.log(firstMax);
+//  console.log(max);
+ 
+ 
+
+//  arr.reduce((a,b)=>{
+//     return 
+    
+//  })
+
+
+
+
+
+
+
+
+
