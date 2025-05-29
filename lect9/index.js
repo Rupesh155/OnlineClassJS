7d34b9fdc27e2667386e43c3ca3c451e259489e4
Rupesh155 <rudhg35@gmail.com>
2025-05-29 10:12:29 +0530

    Hoisting

---
+++ lect9/index.js
@@ -0,0 +1,110 @@
+// let a=5;
+// function sum(){
+//     // return a
+//     console.log(a);
+    
+    
+// }
+//  sum()
+// console.log(b);
+
+
+
+// function outer(){
+//     return [1,2]
+// }
+//  let  a=  outer()
+//  console.log(a);
+ 
+
+// let a=5;
+// function outer(){
+//     function inner(){
+//         console.log(a);
+        
+//     }
+//     // inner()
+//     return inner
+// }  
+// let= b=  outer()
+// b()
+
+//Hoisting
+
+// console.log(a);
+// var a=5   ====>>>>>>        var a 
+//                              console.log(a)
+//                              a=5 
+
+
+// console.log(a);
+// let a=5        //=>  tdz
+// =>>>>     let a 
+//           console.log(a);
+//           a=5
+
+
+
+// console.log(a);    tdz
+// const a=10;    =>>>>>>  const a 
+//                          console.log(a);
+//                           a=10
+
+
+// console.log(a);
+// let a=5
+// let a
+// console.log(a);
+// a=5
+// function sum(){
+//     console.log("hehehehe");
+    
+// }
+// sum()
+// sum() 
+// const sum=()=>{   
+//     console.log("hello");  
+// }  
+// sum()
+// var sum=()=>{
+//     console.log("hehe");  =>>>>>>>>   var sum =undefined
+//                                      sum()
+//                                      sum=()=>{
+
+//                                      }
+     
+// }    
+
+// console.log(a);
+// let a=10
+
+  
+// var a
+// a()
+
+
+// sum()
+// function sum(){
+//     console.log("hehe");     =>>>>    function sum(){
+
+//                                         }  
+//                                         sum()
+    
+// }
+
+
+
+
+
+
+
+
+
+
+          
+
+
+
+
+
+
