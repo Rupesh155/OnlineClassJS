39d8c52666ed5bbcf9c1b9a9755d8aeb60722981
Rupesh155 <rudhg35@gmail.com>
2025-05-29 21:29:45 +0530

    scope

---
+++ lect10/index.js
@@ -0,0 +1,92 @@
+// console.log(a);
+// var a=5   // undefined
+// console.log(b);
+// let b=10    // error
+// sum()
+// function sum(){
+//     console.log("hello");
+    
+// }
+
+// fun1()
+// var  fun1=()=>{
+//     console.log("hiii");
+    
+// }
+// var a = 1;
+// function foo() {
+//   console.log(a);
+//   var a = 2;
+// }
+// foo()
+
+
+
+// var a = 1;
+// function outer() {
+//   console.log(a);
+//   var a = 2;
+//   function inner() {
+//     console.log(a);
+//   }
+//   inner();
+// }
+// outer();
+
+
+
+
+// hoistTest();
+// var hoistTest = function () {
+//   console.log("Hoisted?");
+// };
+
+
+// function foo(){
+//     function bar(){
+//         return 3
+//     }
+//     return bar()
+//     function bar(){
+//         return 8
+//     }
+// }
+// console.log(foo());
+// if(false){
+//     var a=5
+// }
+// console.log(a);
+
+// {
+//    console.log("helloo");
+    
+// }
+// var a=10
+// {
+//     // let a=10;
+//     // console.log(a);
+//     var a=10
+    
+// }
+// console.log(a);
+
+// function sum(){
+//     let a=1
+// }
+// sum()
+// console.log(a);
+// let a=5
+if(false){
+    let a=5
+}
+console.log(a);
+
+
+
+
+
+
+
+
+
+
