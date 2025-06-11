db5ce3b5f6cfead0c8cbf522a245d9af6c2a79b3
Rupesh155 <rudhg35@gmail.com>
2025-06-11 20:46:38 +0530

    web apies

---
+++ lect17/index.js
@@ -0,0 +1,138 @@
+//async programinggggggggg
+
+
+//web api
+
+//    setTimeout(()=>{
+//     console.log("hello");
+
+//    },0) 
+//    setTimeout(()=>{
+//     console.log("bye");
+
+//    },10) 
+// console.log("hiii");    
+
+
+// setInterval(()=>{
+//     console.log("hello");
+    
+// },1000)
+
+// setTimeout(()=>{
+//     console.log("hiii");
+    
+// },1000)
+// i=0;
+// setInterval(()=>{
+//     console.log(i);
+//     i++
+// },1000)
+
+
+// setTimeout(()=>{
+//     console.log('1');
+//     setTimeout(()=>{
+//     console.log('2');
+//         setTimeout(()=>{
+//          console.log('3');
+//             setTimeout(()=>{
+//              console.log('4');
+//              setTimeout(()=>{
+//              console.log('5');   
+// },1000)    
+// },1000)  
+// },1000)
+// },1000)
+// },1000)
+
+
+
+
+// var k=0;
+//   setTimeout(()=>{
+//     for(  k=1;k<=6;k++)
+//         console.log(k);
+        
+//     },k*1000)
+
+
+// function print(i){
+//     setTimeout(()=>{
+//       console.log(i);
+      
+//     },1000*i)
+// }
+// for(var i=1;i<=5;i++){
+//     print(i)
+// }
+
+
+
+
+// let arr=[1,2,3,4,5]
+// for(var i=0;i<=arr.length;i++){
+//     setTimeout(()=>{
+//         console.log(arr[i]);
+        
+
+//     },1000*i)
+    
+// }
+
+// var a=3
+// function foo(){
+//     console.log(a);
+//     console.log(bar());
+//     var a=5
+//     function bar(){
+//         return 4
+//     }   
+// }
+// foo()
+ let time= new Date().toLocaleTimeString()
+ console.log(time);
+// function a(){
+//     var a=5
+//     console.log();
+    
+// }
+
+// function b(){
+//     var a=7
+//     console.log();
+    
+// }
+
+
+// // {                 {
+// i=1,2,3,4,5,6
+//                         }
+//     i=1
+// }
+// {
+//     i=2
+// }
+// {
+//     i=3
+// }
+// {
+//     i=4
+// }
+// {
+//     i=5
+// }
+
+
+{
+    let a=5
+}
+{
+    let a=6
+}
+{
+    var b=1
+}
+{
+    var b=2
+}
