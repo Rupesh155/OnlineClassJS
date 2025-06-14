bb1d7364b585fc96ab670fa21a550cddc107c690
Rupesh155 <rudhg35@gmail.com>
2025-06-14 21:14:17 +0530

    Dom

---
+++ lect20/index.js
@@ -0,0 +1,42 @@
+// D o m
+
+//html + js
+// console.log(document);
+   
+
+
+
+//  let h1=  document.getElementById("one")
+//   let h1=    document.getElementsByClassName('two')
+    //  let h1=    document.querySelector(".two")
+    //  console.log(h1);
+     
+// for(let i of h1){
+//     i.style.color='red'
+// }
+//  h1[0].style.backgroundColor='red'
+//  h1[0].innerText="hiiiii"
+//  console.log(h1[0].innerText);
+
+//   let a=   document.querySelector('a')
+//   console.log(a.getAttribute('href'));
+//   a.setAttribute("href", "http://flipkart.com")
+
+
+
+          const h1=   document.createElement('h1')
+         const div=    document.querySelector('div')
+         h1.innerText="hello"
+         div.append(h1)
+         console.log(div);
+         
+
+
+           
+  
+ 
+
+
+
+ 
+
