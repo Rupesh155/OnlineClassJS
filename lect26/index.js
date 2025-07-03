79aa5ee4fdaf54906614bc0c0318c895992fd8fa
Rupesh155 <rudhg35@gmail.com>
2025-07-03 20:47:51 +0530

    jquery

---
+++ lect26/index.js
@@ -0,0 +1,21 @@
+
+//   let h2=   document.querySelector("h2")
+//   h2.innerText
+
+// 351055824937188
+//   $("h2").css({
+//     color:"red",
+//     border:"1px solid green",
+//   })
+// console.log($("h2").text("hiii"));
+$("button").click(()=>{
+   $("h2").fadeOut()
+    
+})
+
+$("#fadeIn").click(()=>{
+    $("h2").fadeIn()
+     
+ })
+
+
