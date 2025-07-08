42c77c395dbda527e6066e80c687604bd2d1b55d
Rupesh155 <rudhg35@gmail.com>
2025-07-08 20:45:37 +0530

    spread opss

---
+++ lect29/home.js
@@ -0,0 +1,11 @@
+ let user="hellooo"
+
+export default  function share(a,b){
+    console.log(a+b);
+    
+}
+
+export{
+    user
+   
+}
---
+++ lect29/index.js
@@ -0,0 +1,97 @@
+// const obj={
+//     name:"hello",
+//     lastName:"vohra"
+// }
+// let obj1={
+//     ...obj,
+//     age:15
+// // }
+// // console.log(obj1);
+// // let arr=[1,2,4,5,65]
+// // let arr2=[2,3,4,5,...arr]
+// // console.log(arr2);
+
+// // function sum(...data){
+// //     console.log(data);
+    
+// // }
+// // sum(2,3,4,5,6,7,8,86,67,55,5,6)
+
+// // console.log(obj.name);
+
+
+// let obj={
+//     name:"hello",
+//     id:1,
+//     age:45
+// }
+
+// let {age ,id}=obj
+// console.log(age,id);
+
+
+// let arr=[1,2,3,4,5,6]
+// let [a,b,c]=arr
+// console.log(a,b,c);
+
+
+
+
+// let sum= function(){
+//     console.log("hello");
+    
+
+// } 
+// sum()
+
+
+
+// function add(a,b){
+//     return a+b
+
+// }
+
+// console.log(add(5,6));
+// console.log(add(5,6));
+// let count=0
+// function increment(){
+//     count++
+//     return count
+
+// }
+// console.log(increment(), "Impure functionsss");
+// console.log(increment(), "Impure functionsss");
+// console.log(increment(), "Impure functionsss");
+
+
+
+
+
+
+
+
+// function add(a){
+//     return function(b){
+//         console.log(a+b);   
+//     }
+// }
+// add(2)(3)
+
+import {user} from './home.js'
+import a from './home.js'
+console.log(user);
+a(5,4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
