1f03b601acb63d21113cdb63c691d32dfb403733
Rupesh155 <rudhg35@gmail.com>
2025-07-10 20:54:23 +0530

    react start

---
+++ lect31/my-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+const App = () => {
+  return (
+    <div>
+      <h2> hello</h2>
+      <h2> hello</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, nisi?</p>
+    </div>
+
+
+    
+  )
+}
+
+export default App
