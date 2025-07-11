b32c5ae49d74daf0232bf6e8668d659e073f056c
Rupesh155 <rudhg35@gmail.com>
2025-07-11 20:45:00 +0530

    code

--- lect31/my-react-app/src/App.jsx
+++ lect31/my-react-app/src/App.jsx
@@ -1,16 +1,39 @@
+import React from 'react'
+import './App.css'
 
 
 const App = () => {
-  return (
-    <div>
-      <h2> hello</h2>
-      <h2> hello</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia, nisi?</p>
-    </div>
-
-
+  let arr=[1,2,3,4,5] 
+  function fun1(){
+    console.log("hehehehe");
     
+  }
+  return (
+    // <div>
+    // <button  onClick={fun1}>click</button>
+    // </div>
+    <div>App 
+      {
+        arr.map((a)=>{
+          return(<>
+          <h1> {a}</h1>
+          </>)
+        })
+      }
+    </div>
   )
 }
+
+
+// const Home=()=>{
+//   return(
+//     <div>
+//       <h2> Home Compoo</h2>
+//     </div>
+//   )
+
+// }
+// export {Home}
+
 
 export default App
