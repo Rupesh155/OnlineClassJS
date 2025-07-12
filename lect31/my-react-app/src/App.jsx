37e62f1e1ecfae7ef54a86bc1a85210524f5616a
Rupesh155 <rudhg35@gmail.com>
2025-07-12 20:55:01 +0530

    state

--- lect31/my-react-app/src/App.jsx
+++ lect31/my-react-app/src/App.jsx
@@ -1,39 +1,55 @@
-import React from 'react'
-import './App.css'
+// import React from 'react'
+// import './App.css'
 
+
+// const App = () => {
+//   let arr=[1,2,3,4,5] 
+//   function fun1(){
+//     console.log("hehehehe");
+    
+//   }
+//   return (
+//     // <div>
+//     // <button  onClick={fun1}>click</button>
+//     // </div>
+//     <div>App 
+//       {
+//         arr.map((a)=>{
+//           return(<>
+//           <h1> {a}</h1>
+//           </>)
+//         })
+//       }
+//     </div>
+//   )
+// }
+
+
+// // const Home=()=>{
+// //   return(
+// //     <div>
+// //       <h2> Home Compoo</h2>
+// //     </div>
+// //   )
+
+// // }
+// // export {Home}
+
+
+// export default App
+
+
+
+
+import React from 'react'
+import Day2 from './Day2'
 
 const App = () => {
-  let arr=[1,2,3,4,5] 
-  function fun1(){
-    console.log("hehehehe");
-    
-  }
   return (
-    // <div>
-    // <button  onClick={fun1}>click</button>
-    // </div>
-    <div>App 
-      {
-        arr.map((a)=>{
-          return(<>
-          <h1> {a}</h1>
-          </>)
-        })
-      }
+    <div>
+      <Day2/>
     </div>
   )
 }
-
-
-// const Home=()=>{
-//   return(
-//     <div>
-//       <h2> Home Compoo</h2>
-//     </div>
-//   )
-
-// }
-// export {Home}
-
 
 export default App
---
+++ lect31/my-react-app/src/Day2.jsx
@@ -0,0 +1,43 @@
+// import React, { useState } from 'react'
+
+// const Day2 = () => {
+//    let [count ,SetCount]=     useState(0)
+//    let [color ,SetColor]=     useState(true)
+//     function fun1(){
+//         SetCount(count+1)  
+//     }
+//     function  fun2(){
+//         SetColor(!color)
+//     } 
+//   return (
+//     <div style={{backgroundColor:color?"red":"green",height:"400px"}}>
+//       <h2> {count}</h2>
+//     <button onClick={fun1} > Incr</button>
+//     <button onClick={()=>SetCount(count-1)} > decr</button>
+//     <button onClick={()=>SetCount(0)} > reset</button>
+//     <button onClick={fun2} > change</button>
+//     </div>
+//   )
+// }
+
+// export default Day2
+
+
+
+import React, { useState } from 'react'
+
+const Day2 = () => {
+    const [input,SetInput]=     useState("")
+    function fun1(e){
+        SetInput(e.target.value)
+        
+    }
+  return (
+    <div>
+        <h2>{input}</h2>
+        <input    onChange={fun1} type='text'  placeholder='Enter your name'/>
+    </div>
+  )
+}
+
+export default Day2
