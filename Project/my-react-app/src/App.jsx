688038a291e4f7ee722e041fc9475186a49faba6
Rupesh155 <rudhg35@gmail.com>
2025-09-14 20:14:44 +0530

    connect

--- Project/my-react-app/src/App.jsx
+++ Project/my-react-app/src/App.jsx
@@ -68,13 +68,13 @@
 
 // useMemo or useCallback
 
-import React, { memo, useCallback, useMemo, useState } from 'react'
+// import React, { memo, useCallback, useMemo, useState } from 'react'
 
-const App = () => {
-  let [count, setCount] = useState(0)
-  let [state, setState] = useState(0)
+// const App = () => {
+//   let [count, setCount] = useState(0)
+//   let [state, setState] = useState(0)
 
-  console.log("apppp wali fileeeee");
+//   console.log("apppp wali fileeeee");
 
 
   //  let val=   useMemo(()=>{
@@ -110,51 +110,215 @@ const App = () => {
   //   }
   // },[])
 
- let even=  useCallback(()=>{
-  console.log("hello");
- },[])
-  return (
-    <div>
-      {/* <h1>{val}</h1> */}
-      <h1>appp</h1>
-      <h2 >{count}</h2>
-      <button onClick={() => setCount(count + 1)}>click</button>
-      <button onClick={() => setState(state - 1)}>--</button>
+//  let even=  useCallback(()=>{
+//   console.log("hello");
+//  },[])
+//   return (
+//     <div>
+//       {/* <h1>{val}</h1> */}
+//       <h1>appp</h1>
+//       <h2 >{count}</h2>
+//       <button onClick={() => setCount(count + 1)}>click</button>
+//       <button onClick={() => setState(state - 1)}>--</button>
 
-      {/* <ChildApp /> */}
-      <MemoizedChild   even={even} />
-    </div>
-  )
-}
+//       {/* <ChildApp /> */}
+//       <MemoizedChild   even={even} />
+//     </div>
+//   )
+// }
 
-export default App
+// export default App
 
 
-  let MemoizedChild=   memo(function ChildApp() {
-    console.log("childdddddd wali fileeeee");
-  return (
-    <div>Childddddd
-      <h2></h2>
-    </div>
-  )
-
-})  
-
-// const ChildApp = () => {
-//   console.log("childdddddd wali fileeeee");
-
+//   let MemoizedChild=   memo(function ChildApp() {
+//     console.log("childdddddd wali fileeeee");
 //   return (
 //     <div>Childddddd
 //       <h2></h2>
 //     </div>
 //   )
+
+// })  
+
+// // const ChildApp = () => {
+// //   console.log("childdddddd wali fileeeee");
+
+// //   return (
+// //     <div>Childddddd
+// //       <h2></h2>
+// //     </div>
+// //   )
+// // }
+
+
+
+
+
+
+
+
+// import React, { useEffect, useRef, useState } from 'react'
+
+// const App = () => {
+//   let [myData,SetMyData]=useState("");
+//     // let [count,setCount]=useState()
+//        let data=    useRef(0)
+//        console.log(data,"kya mil rha hai ");
+       
+//   useEffect(()=>{
+
+//     data.current=data.current+1;
+//     // setCount(count+1)
+
+//   })
+
+//   return (
+//     <div>
+//       <input  type='text'  value={myData} onChange={(e)=>SetMyData(e.target.value)}/>
+//       <p> number of times render : {data.current}</p>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+// import React, { useRef } from 'react'
+
+// const App = () => {
+//       // let data=   document.getElementById("one")
+//       // console.log(data,"heheh");
+
+//       let data=   useRef(null)
+
+//       function done(){
+//         let val=data.current.value
+//         console.log(val,"msg");
+//       }
+      
+//   return (
+//     <div>
+//       <input   ref={data}  id='one' name=''  />
+//       <button  onClick={done}>click</button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+// import React from "react";
+// import { motion } from "framer-motion";
+
+// export default function App() {
+//   return (
+//     <div style={{ display: "flex", justifyContent: "center", marginTop: "50px" }}>
+//       <motion.div
+//         initial={{ opacity: 0, x: -100 }}   // pehle hidden
+//         animate={{ opacity: 1, x: 0 }}      // entry ke time slide in
+//         transition={{ duration: 0.8 }}
+//         whileHover={{ scale: 1.1 }}         // hover par bada hoga
+//         style={{
+//           width: "200px",
+//           height: "120px",
+//           background: "linear-gradient(135deg, #6a11cb 0%, #2575fc 100%)",
+//           borderRadius: "20px",
+//           display: "flex",
+//           alignItems: "center",
+//           justifyContent: "center",
+//           color: "white",
+//           fontSize: "20px",
+//           cursor: "pointer"
+//         }}
+//       >
+//         Framer Motion 🚀
+//       </motion.div>
+//     </div>
+//   );
 // }
 
 
 
+// import React, { lazy, Suspense, useState } from 'react'
+// // import LazyLoading from './LazyLoading'
+// const LazyC=lazy(()=>import('./LazyLoading'))
+
+// const App = () => {
+//   let [show,SetShow]=useState(false)
+
+//   return (
+//     <div>
+        
+//      {/* {show?<LazyLoading/>:null}  */}
+//         {
+//           show&&(<Suspense fallback="Loading..."> <LazyC/>   </Suspense>)
+//         }
+//          <button onClick={()=>SetShow(true)} >click</button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+import React, { useState } from 'react' 
+
+const App = () => {
+  let [formData,SetFormData]=useState({
+    name:"",
+    passWord:""
+  })          
+  function handleForm(e){
+    let {name,value}=e.target
+    SetFormData({
+      ...formData,[name]:value
+    })
+  }
+
+    async  function handleSubmit(e){
+    e.preventDefault()
+    // console.log(formData);
+     
+     let data=     await fetch("http://localhost:4000/create" ,{
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json"
+      },
+      body:JSON.stringify(formData)
+    })
+    // let res=    await data.json()
+    // console.log(res);
+  
+    // console.log("heheh");
+    
 
 
+    
+  }
 
+  // {
+  //   name:"rimi",
+  //   passWord:"123"    
+  // }
+  // {
+  //   "name":"123",
+  //   "dsifwi":2423
+  // }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input  name='name' value={formData.name}  onChange={handleForm} type='text' placeholder='Name'/>
 
+        <br></br>
+        <br></br>
+        <input   name='passWord' value={formData.passWord}  onChange={handleForm} type='password' placeholder='password'/>
+        <br></br>
+        <br></br>
+        <button type='submit'> AddData</button>
 
+      </form>
+    </div>
+  )
+}
 
+export default App
